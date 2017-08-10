49d8f1f0a10409576d08d133978fdbc6b0f2bf38
Alfred Holmes <holmesey55@gmail.com>
2017-08-10 23:15:33 +0100

    Added error reporting for untracked coins

--- prices.py
+++ prices.py
@@ -73,8 +73,10 @@ def get_data(timestamp):
 
             data = json.loads(requests.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=' + curr + '&tsyms=' + comma_key_set + '&ts=' + str(timestamp)).text)[curr]
             for key, price in data.items():
-                d[curr + '_' + key] = price**-1
-
+                if price != 0:
+                    d[curr + '_' + key] = price**-1
+                else:
+                    print('Error, 0 returned from cryptocompare with pair: ' + curr + "_" + key + ', this means that this coin is not tracked')
     return d
 
 if __name__ == '__main__':
